f1c99cb821dbbaf675ddcc6f0c34d76e4654829d
Eliasgdr <eliasgaudare@gmail.com>
2023-09-21 01:13:05 +0200

    Données restructurées (fusion Circles et Balls), collision pas réparée et erreur de segmentation mystère

--- main.cpp
+++ main.cpp
@@ -7,10 +7,8 @@
 int main(){
 //Setup
     Ball balls[NUM_BALLS]; //Tableau contenant toutes les balles
-    CircleShape circles[NUM_BALLS]; //Tableau contenant les données graphiques des balles
 
     randomBalls(balls); //Rempli le tableau de balles aux valeurs aléatoires
-
     RenderWindow window(sf::VideoMode(MAX_X, MAX_Y), "2D Simulation"); //Initialise la fenêtre de rendu
 
     std::vector<int> grid[NUM_CASES_X][NUM_CASES_Y]; // Créer une grille 3D de taille variable sur l'axe z
@@ -21,13 +19,14 @@ int main(){
     rectangle.setOutlineThickness(10);
     rectangle.setOutlineColor(Color(212, 212, 212));
 
+    /*
     //Créer la grille visuelle
     int num_segments = NUM_CASES_X + NUM_CASES_Y - 2;
     sf::VertexArray visual_grid(sf::Lines, 2*(num_segments));
     // row separators
     for(int i=0; i < NUM_CASES_Y-1; i++){
         int r = i+1;
-        float rowY = CELL_SIZE*r;
+        auto rowY = (float) (CELL_SIZE*r);
         visual_grid[i*2].position = {0, rowY};
         visual_grid[i*2+1].position = {MAX_X, rowY};
     }
@@ -35,32 +34,45 @@ int main(){
 
     for(int i=NUM_CASES_Y-1; i < num_segments; i++){
         int c = i-NUM_CASES_Y+2;
-        float colX = CELL_SIZE*c;
+        auto colX = (float) (CELL_SIZE*c);
         visual_grid[i*2].position = {colX, 0};
         visual_grid[i*2+1].position = {colX, MAX_Y};
-    }
+    }*/
 
 
 
     Clock clock;
-    //window.setFramerateLimit(60);
+    window.setFramerateLimit(60);
     Time t;
 
-    const int relative_grid[3][2] = {{0,1}, //Utile plus tard pour parcourir les 3 cases à droite et en bas de la case actuelle
+    const int relative_grid[8][2] = {{-1,-1}, //Utile plus tard pour parcourir les 3 cases à droite et en bas de la case actuelle
+                                    {-1,0},
+                                    {-1,1},
+                                    {0,-1},
+                                    {0,1},
+                                    {1,-1},
                                     {1,0},
                                     {1,1}};
 
 //Boucle principale
+    int frame = 0;
     while (window.isOpen())
     {
-        Event event;
+        frame++;
+        Event event{};
         clock.restart().asSeconds();
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
 
         window.clear();
         window.draw(rectangle);
-        window.draw(visual_grid);
+        //window.draw(visual_grid);
 
-        placeBalls(balls,grid,circles);
+        placeBalls(balls,grid);
 
         for(int ix=0;ix<NUM_CASES_X-1;ix++){ //Parcours toutes les cases de la grille
             for(int iy=0;iy<NUM_CASES_Y-1;iy++){
@@ -75,8 +87,8 @@ int main(){
                     }
 
                     //Collision avec les balles dans d'autres cases
-                    for(auto relative_pos : relative_grid){
-                        std::vector<int> other_vector = grid[ix + relative_pos[0]][iy + relative_pos[1]]; //Parcours les 3 cases à droite et en bas
+                    for(int r=0; r<8; r++){
+                        std::vector<int> other_vector = grid[ix + relative_grid[r][0]][iy + relative_grid[r][1]]; //Parcours les 3 cases à droite et en bas
                         if (other_vector.empty()){
                             continue; //Passe à l'itération suivante si le vecteur est vide
                         }
@@ -88,23 +100,27 @@ int main(){
             }
         }
 
-        for (int i=0;i<NUM_BALLS;i++) {
-            circles[i].setPosition(balls[i].pos.x - balls[i].radius, balls[i].pos.y - balls[i].radius);
-            window.draw(circles[i]);
-            balls[i].update(t.asSeconds());
+
+        /*
+        for (int i=0;i<NUM_BALLS;i++){
+            for (int j=i+1;j<NUM_BALLS;j++){
+                collision(&balls[i],&balls[j]);
+            }
+        }*/
+
+        for (int a=0;a<NUM_BALLS;a++){
+            balls[a].update(t.asSeconds());
+            //std::cout<<"b.x = "<<balls[a].pos.x <<" c.x = "<<circles[a].getPosition().x<<'\n';
+            window.draw(balls[a].getCircle());
         }
         //Changer les positions des balles dans la grille
 
 
-        while (window.pollEvent(event))
-        {
-            if (event.type == Event::Closed)
-                window.close();
-        }
+
 
 
         window.display();
         t = clock.getElapsedTime();
-        //std::cout << 1.0 / t.asSeconds() << " fps\n";
+        std::cout << 1.0 / t.asSeconds() << " fps\n";
     }
 }
--- objects.h
+++ objects.h
@@ -6,104 +6,172 @@
 
 #define MAX_X 1280.0f
 #define MAX_Y 720.0f
-#define MAX_SPEED 100.0f
-#define MIN_RADIUS 30.0f
+#define MAX_SPEED 200.0f
+#define MIN_RADIUS 20.0f
 #define MAX_RADIUS 50.0f //Bizarre je peux pas mettre à 2.0f
-#define GRAVITY 0.0f
-#define FRICTION 0.8f
-#define NUM_BALLS 4 //Bizarre je peux pas augmenter
+#define GRAVITY 800.0f
+#define FRICTION 0.9f
+#define NUM_BALLS 20 //Bizarre je peux pas augmenter
 
 const int CELL_SIZE = 4 * (int)MAX_RADIUS;
 const int NUM_CASES_X = MAX_X / CELL_SIZE;
 const int NUM_CASES_Y = MAX_Y / CELL_SIZE;
 
-
-class Point{
-    public:
-        Vector2f pos;
-        Vector2f speed;
-
-        Point(){
-            pos = Vector2f();
-            speed = Vector2f();
-        }
-
-        Point(float x, float y, float sx, float sy){
-            pos = Vector2f(x,y);
-            speed = Vector2f(sx,sy);
-        }
-};
-
-class Ball : public Point {
-    public:
-        float radius;
+class Ball{
+    private:
+        CircleShape circle;
+        Vector2f velocity;
         float mass;
-        Uint8 color[3]{};
 
-        Ball() : Point() {
-            radius = 0.0f;
-            mass = 0.0f;
-            color[0] = 0;
-            color[1] = 0;
-            color[2] = 0;
+    public:
+        Ball(){
+            circle = CircleShape();
+            velocity = Vector2f();
+            mass=0;
+        }
+        Ball(float x, float y, float vx, float vy, float r, Uint8 rgb[3]) {
+            setPos(x,y);
+            setVel(vx,vy);
+            setRadius(r);
+            setColor(rgb[0],rgb[1],rgb[2]);
+
         }
 
-        Ball(float x, float y, float sx, float sy, float r, Uint8 rgb[3]) : Point(x, y, sx, sy) {
-            radius = r;
-            mass = 3.14f*r*r;
-            color[0] = rgb[0];
-            color[1] = rgb[1];
-            color[2] = rgb[2];
+        Vector2f getPos(){
+            return circle.getPosition();
+        }
 
+        Vector2f getVel(){
+            return velocity;
+        }
+
+        float getRadius(){
+            return circle.getRadius();
+        }
+
+        float getMass() const{
+            return mass;
+        }
+
+        CircleShape getCircle(){
+            return circle;
+        }
+
+        void setPos(float x, float y){
+            if(_isnan(x) || _isnan(y)){
+                std::cout<<"setPos : value error : x="<<x<<" y="<<y<<'\n';
+                return;
+            }
+            circle.setPosition(x,y);
+        }
+
+        void setPos(Vector2f v){
+            if(_isnan(v.x) || _isnan(v.y)){
+                std::cout<<"setPos : value error : x="<<v.x<<" y="<<v.y<<'\n';
+                return;
+            }
+            circle.setPosition(v.x,v.y);
+        }
+
+        void setVel(float x, float y){
+            if(_isnan(x) || _isnan(y)){
+                std::cout<<"setVel : value error : vx="<<x<<" vy="<<y<<'\n';
+                return;
+            }
+            velocity.x = x;
+            velocity.y = y;
+        }
+
+        void setVel(Vector2f v) {
+            if (_isnan(v.x) || _isnan(v.y)) {
+                std::cout << "setPos : value error : vx="<<v.x<<" vy="<<v.y<<'\n';
+                return;
+            }
+            velocity.x = v.x;
+            velocity.y = v.y;
+        }
+
+        void setRadius(float r){
+            if(_isnan(r) || r < MIN_RADIUS || r > MAX_RADIUS){
+                std::cout<<"setRadius : value error : r="<<r<<'\n';
+                return;
+            }
+            circle.setRadius(r);
+            circle.setOrigin(r, r);
+            mass = 3.1415f*r*r;
+        }
+
+        void setColor(int r, int g, int b){
+            if(_isnan(r) || _isnan(g) || _isnan(b) || r < 0 || g < 0 || b < 0 || r > 255 || g > 255 || b > 255){
+                std::cout<<"setColor : value error : r="<<r<<" g="<<g<<" b="<<b<<'\n';
+                return;
+            }
+            circle.setFillColor(Color(r,g,b));
         }
 
         void update(float delta_sec){
-            speed.y += GRAVITY * delta_sec;
-            pos += speed * delta_sec;
-            if(pos.x - radius < 0.0f){
-                pos.x += 2.0f * (radius - pos.x);
-                speed.x *= FRICTION * -1.0f;
+            if(delta_sec<=0){
+                return;
             }
-            else if(pos.x + radius >= MAX_X){
-                pos.x -= 2.0f * (pos.x + radius - MAX_X);
-                speed.x *= FRICTION * -1.0f;
+            Vector2f vel = getVel();
+            Vector2f pos = getPos();
+            float r = getRadius();
+            vel.y += GRAVITY * delta_sec;
+            pos += vel * delta_sec;
+            if(pos.x - r < 0.0f){
+                pos.x += 2.0f * (r - pos.x);
+                vel.x *= FRICTION * -1.0f;
             }
-            if(pos.y - radius < 0.0f){
-                pos.y += 2.0f * (radius - pos.y);
-                speed.y *= FRICTION * -1.0f;
+            else if(pos.x + r >= MAX_X){
+                pos.x -= 2.0f * (pos.x + r - MAX_X);
+                vel.x *= FRICTION * -1.0f;
             }
-            else if(pos.y + radius >= MAX_Y){
-                pos.y -= 2.0f * (pos.y + radius - MAX_Y);
-                speed.y *= FRICTION * -1.0f;
+            if(pos.y - r < 0.0f){
+                pos.y += 2.0f * (r - pos.y);
+                vel.y *= FRICTION * -1.0f;
             }
-
+            else if(pos.y + r >= MAX_Y){
+                pos.y -= 2.0f * (pos.y + r - MAX_Y);
+                vel.y *= FRICTION * -1.0f;
+            }
+            if(_isnan(pos.x)){
+                std::cout<<"update NaN : "<<delta_sec<<'\n';
+            }
+            setPos(pos.x,pos.y);
+            setVel(vel.x,vel.y);
         }
 };
 
 
 bool collision(Ball* b1, Ball* b2){
-    std::cout << "xo";
-    Vector2f normal = b2->pos - b1->pos;
-    float distance = length(normal);
-    if (distance <= b1->radius + b2->radius) {
-        std::cout << "\nCOLLISION\n";
-        Vector2f collision_normal = normalize(normal);
+    float sum_radius = b1->getRadius() + b2->getRadius();
+    Vector2f dist = b1->getPos() - b2->getPos();
+    float dist_squared = dist.x * dist.x + dist.y * dist.y;
+    if(_isnan(b1->getPos().x)){
+            std::cout<<"collision NaN\n";
+    }
+    if( dist_squared <= sum_radius * sum_radius){ //Comparer la distance au carré est plus rapide
 
-        // Relative velocity along the collision normal
-        Vector2f relativeVelocity = b2->speed - b1->speed;
-        float collision_speed = dotProduct(relativeVelocity, collision_normal);
+        Vector2f normal_vect = b1->getPos() - b2->getPos(); // Vecteur n
+        Vector2f unit_vect = normal_vect / sqrtf(normal_vect.x * normal_vect.x + normal_vect.y * normal_vect.y); //Vecteur un
+        Vector2f tangent_vect = Vector2f(-1.0f * unit_vect.y, unit_vect.x); //Vecteur ut
 
-        // Impulse calculation
-        float impulse = (2.0f * collision_speed) / (b1->mass + b2->mass);
+        float b1_norm = dotProduct(unit_vect, b1->getVel()); //Scalaire v1n
+        float b1_tangent = dotProduct(tangent_vect, b1->getVel()); //Scalaire v1t
+        float b2_norm = dotProduct(unit_vect, b2->getVel()); //Scalaire v2n
+        float b2_tangent = dotProduct(tangent_vect, b2->getVel()); //Scalaire v2t
 
-        // Apply impulse to velocities
-        b1->speed += impulse * b2->mass * collision_normal;
-        b2->speed -= impulse * b1->mass * collision_normal;
+        float b1_norm_new = FRICTION * (b1_norm * (b1->getMass() - b2->getMass()) + 2 * b2->getMass() * b2_norm) / (b1->getMass() + b2->getMass());
+        float b2_norm_new = FRICTION * (b2_norm * (b2->getMass() - b1->getMass()) + 2 * b1->getMass() * b1_norm) / (b1->getMass() + b2->getMass());
 
-        // Move the balls to avoid overlap
-        float overlap = (b1->radius + b2->radius) - distance;
-        b1->pos -= overlap * 0.5f * collision_normal;
-        b2->pos += overlap * 0.5f * collision_normal;
+        b1->setVel((b1_norm_new * unit_vect) + (b1_tangent * tangent_vect));
+        b2->setVel((b2_norm_new * unit_vect) + (b2_tangent * tangent_vect));
+
+        // Pour être sûr que les balles ne restent pas en collision
+        float dist_to_move = sum_radius - sqrtf(dist_squared);
+        float angle = atan2f(b2->getPos().y - b1->getPos().y, b2->getPos().x - b1->getPos().x);
+        b2->setPos(b2->getPos().x + cosf(angle) * dist_to_move / 2, b2->getPos().y + sinf(angle) * dist_to_move / 2);
+        b1->setPos(b1->getPos().x + cosf(angle + (float) M_PI) * dist_to_move / 2, b1->getPos().y + sinf(angle + (float) M_PI) * dist_to_move / 2);
         return true;
     }
     return false;
@@ -114,28 +182,24 @@ void randomBalls(Ball balls[]){
     std::mt19937 gen(rd());
     for(int i=0;i<NUM_BALLS;i++){
         std::uniform_real_distribution<float> dis_radius(MIN_RADIUS, MAX_RADIUS);
-        balls[i].radius = dis_radius(gen);
-        balls[i].mass = M_PI * balls[i].radius * balls[i].radius;
+        balls[i].setRadius(dis_radius(gen));
 
-        std::uniform_real_distribution<float> dis_x(balls[i].radius, MAX_X - balls[i].radius);
-        std::uniform_real_distribution<float> dis_y(balls[i].radius, MAX_Y - balls[i].radius);
-        balls[i].pos = Vector2f(dis_x(gen), dis_y(gen));
+        std::uniform_real_distribution<float> dis_x(balls[i].getRadius(), MAX_X - balls[i].getRadius());
+        std::uniform_real_distribution<float> dis_y(balls[i].getRadius(), MAX_Y - balls[i].getRadius());
+        balls[i].setPos(dis_x(gen), dis_y(gen));
 
         std::uniform_real_distribution<float> dis_speed(-MAX_SPEED, MAX_SPEED);
-        balls[i].speed = Vector2f(dis_speed(gen), dis_speed(gen));
+        balls[i].setVel(dis_speed(gen), dis_speed(gen));
 
         std::uniform_int_distribution<Uint8> dis_color(160, 255);
-        balls[i].color[0] = dis_color(gen);
-        balls[i].color[1] = dis_color(gen);
-        balls[i].color[2] = dis_color(gen);
+        balls[i].setColor(dis_color(gen), dis_color(gen), dis_color(gen));
     }
 }
 
-void placeBalls(Ball balls[],std::vector<int> grid[NUM_CASES_X][NUM_CASES_Y], CircleShape circles[]){
+void placeBalls(Ball balls[],std::vector<int> grid[NUM_CASES_X][NUM_CASES_Y]){
     int taille;
     for(int x=0;x<NUM_CASES_X;x++){
         for(int y=0;y<NUM_CASES_Y;y++){
-            taille = grid[x][y].size();
             grid[x][y].clear();
             grid[x][y].shrink_to_fit();
 
@@ -144,17 +208,21 @@ void placeBalls(Ball balls[],std::vector<int> grid[NUM_CASES_X][NUM_CASES_Y], Ci
 
     int grid_x, grid_y;
     for(int i=0;i<NUM_BALLS;i++){
-        grid_x = (int)balls[i].pos.x / CELL_SIZE;
-        grid_y = (int)balls[i].pos.y / CELL_SIZE;
+        grid_x = (int)balls[i].getPos().x / CELL_SIZE;
+        grid_y = (int)balls[i].getPos().y / CELL_SIZE;
+        if(_isnan(balls[i].getPos().x)){
+            std::cout<<"placeBalls NaN\n";
+        }
+
         if(grid_x>=NUM_CASES_X){
             grid_x = NUM_CASES_X - 1;
         }
-        if(grid_y>=NUM_CASES_X){
-            grid_y = NUM_CASES_X - 1;
+        if(grid_y>=NUM_CASES_Y){
+            grid_y = NUM_CASES_Y - 1;
         }
+
         grid[grid_x][grid_y].push_back(i); //Rempli la grille des indices de toutes les balles en fonction de leur position
-        circles[i].setRadius(balls[i].radius);
-        circles[i].setFillColor(Color(balls[i].color[0],balls[i].color[1],balls[i].color[2]));
+
     }
 }
 
